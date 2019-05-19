1cf11a90e7d74a791f6b4d72c6f958a0d043e7c7
Tezikov Roman <tez.romach@gmail.com>
2019-05-19 21:51:31 +0300

    remove unused

--- safitty/__init__.py
+++ safitty/__init__.py
@@ -1,5 +1,5 @@
 from safitty.core import get, set, safe_set, safe_get
-from safitty.types import star, dstar, Storage, Key, Transform
+from safitty.types import Storage, Key, Transform
 from safitty.parser import argparser, load, load_config, \
     update_config_from_args, load_from_args, load_config_from_args
 
