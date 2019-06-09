70be472b6fef8805962c44858684e7a7736615a9
Tezikov Roman <tez.romach@gmail.com>
2019-06-09 16:30:50 +0300

    update setter

--- safitty/dict.py
+++ safitty/dict.py
@@ -1,32 +1,41 @@
 import collections
 import copy as dcopy
-from typing import Iterator, Union, Tuple
+
+from typing import Iterator, Union, Any
 from pathlib import Path
-from .types import Storage, Key
 
 from . import core
 from . import parser
+from .types import Storage, Key, Keys
 
 
 class Safict(collections.Mapping):
     def __init__(
         self,
         storage: Storage = None,
-        separator=None,
+        separator: str = None,
     ):
         self._storage = storage or {}
+        self._original_storage = dcopy.deepcopy(self._storage)
         self.separator = separator
 
-    def get(self, *keys: Key, **get_params) -> Storage:
+    def _split_keys(self, keys: Keys) -> Keys:
         _keys = []
         if self.separator is not None:
             for key in keys:
-                split = key.split(self.separator)
-                for elem in split:
+                if type(key) == str:
+                    splits = key.split(self.separator)
+                else:
+                    splits = [key]
+                for elem in splits:
                     _keys.append(elem)
         else:
             _keys = keys
 
+        return _keys
+
+    def get(self, *keys: Key, **get_params) -> Storage:
+        _keys = self._split_keys(keys)
         result: Storage = core.get(self._storage, *_keys, **get_params)
         return result
 
@@ -34,13 +43,16 @@ class Safict(collections.Mapping):
         return dcopy.deepcopy(self._storage)
 
     def set(self, *keys: Key, value, **set_params) -> 'Safict':
+        _set_params = parser.update(dict(inplace=False), set_params)
+        _keys = self._split_keys(keys)
+
         result: Storage = core.set(
             self._storage,
-            *keys, **set_params,
-            value=value, inplace=False
+            *_keys, **_set_params,
+            value=value
         )
 
-        return Safict(result)
+        return Safict(result, separator=self.separator)
 
     @staticmethod
     def load(
@@ -67,21 +79,27 @@ class Safict(collections.Mapping):
             indent=indent,
         )
 
-    def copy(self, separator=None) -> 'Safict':
+    def copy(self) -> 'Safict':
         storage = dcopy.deepcopy(self._storage)
-        result = Safict(storage, separator)
+        result = Safict(storage, self.separator)
 
         return result
 
-    def __copy__(self, separator=None):
-        return self.copy(separator)
+    def __copy__(self):
+        return self.copy()
 
-    def __getitem__(self, keys: Union[Key, Tuple[Key, ...]]) -> Storage:
+    def __getitem__(self, keys: Union[Key, Keys]) -> Storage:
         if type(keys) == tuple:
             result = self.get(*keys)
         else:
             result = self.get(keys)
         return result
+
+    def __setitem__(self, keys: Union[Key, Keys], value: Any) -> None:
+        if type(keys) == tuple:
+            self.set(*keys, value=value, inplace=True)
+        else:
+            self.set(keys, value=value, inplace=True)
 
     def __len__(self) -> int:
         return self._storage.__len__()
